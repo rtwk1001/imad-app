20c9e449a53902198d9a1747115f2dac96f6d522
rtwk1001 <rtwk1001@gmail.com>
2017-08-17 02:17:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,3 +22,11 @@ button.onclick=function(){
 };
 var submit=document.getElementById("submit-res");
 var text=document.getElementById("name");
+submit.onclick=function(){
+    var names=["name1","name2","name3","name4"];
+    var list="";
+    for(var i=0;i<names.length;i++)
+    list=list+"<li>"+names[i]+"</li>";
+    var ul=document.getElementById("namelist");
+    ul.innerHTML=list;
+};
