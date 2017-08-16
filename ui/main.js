840de66be61b2202175da9c6fe147bec707a534f
rtwk1001 <rtwk1001@gmail.com>
2017-08-17 00:41:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 console.log('Loaded!');
+var img=document.getElementById('madi');
+var marginleft=0;
+function moveRight(){
+    while(margin<=50){
+    marginleft+=5;
+    
+    img.style.marginLeft=marginleft+"px";}
+    
+}
+img.onclick=function(){
+    var interval=setInterval(moveright,50);
+    
+};
