fc0d305f730eb083ce241a85c810a73f4b4841f1
rtwk1001 <rtwk1001@gmail.com>
2017-09-13 19:10:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/persons/:personName', function (req, res) {
 
 });
 function hash(input, salt){
-    var hashed =crpto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    var hashed =crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return hashed.toString('hex');
 }
 app.get('/hash/:input',function(req,res){
