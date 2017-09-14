36db66e2a1e4eb50db0f245f78e7cb1b0c7575bc
rtwk1001 <rtwk1001@gmail.com>
2017-09-14 17:25:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
 });
 function hash(input, salt){
     var hashed =crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return ["pbkdg2","10000",salt,hashed.toString('hex')].join('$');
+    return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
 }
 app.get('/hash/:input',function(req,res){
     var hashstring=hash(req.params.input,'some-random-string');
