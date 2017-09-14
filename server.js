617ce5d489e2c23fb5768afe9cd5a54f3cc0d05a
rtwk1001 <rtwk1001@gmail.com>
2017-09-14 17:00:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -50,7 +50,13 @@ app.post('/login',function(req,res){
             if(result.rows.length===0)
             res.status(403).send("username aur password Invalid");
             else{
-                
+                var dbString=result.rows[0].password;
+                var salt=dbString.split('$')[2];
+                var hashedpassword=hash(password,salt);//creating hash according to user input
+                if(hashedpassword===dbString)
+                res.send("Credentials are correct!");
+                else
+                res.status(403).send("username aur password Invalid");
             }
         }
        
