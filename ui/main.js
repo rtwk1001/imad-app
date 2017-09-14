df27433358d19a37eeb2f2cc70c4d13df3520d62
rtwk1001 <rtwk1001@gmail.com>
2017-09-14 17:33:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,7 +22,8 @@ submit.onclick=function(){
      };
      var username=document.getElementById("username").value;
          var password=document.getElementById("password").value;
-        
+        console.log("username:"+username);
+        console.log("password:"+password);
          
     request.open('POST',"http://rtwk1001.imad.hasura-app.io/login",true);
     request.setRequestHeader('content-type','application/json');
