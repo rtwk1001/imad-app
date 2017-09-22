d6688edc2fb60b0d6b4be38f59f2d2f7e8a2a77d
Ritwik Jain <rtwk1001@gmail.com>
2017-09-23 02:31:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -236,7 +236,7 @@ var port = 8080; // Use 8080 for local development because you might already hav
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
-=======
+
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
