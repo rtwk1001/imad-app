1c15bbcf760c0200689d0f163c5e8e84505d7e92
Ritwik Jain <rtwk1001@gmail.com>
2017-09-23 02:39:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,8 +20,7 @@ app.use(bodyParser.json());
 app.use(session({
     secret: 'someRandomSecretValue',
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
-     resave: true,
-    saveUninitialized: true
+    
 }));
 
 function createTemplate (data) {
